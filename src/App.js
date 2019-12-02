bc6a0cc43f3a1134ee34d5b3a2e402f1f1d79a63
Robert Gordon <Komeuppance@gmail.com>
2019-12-01 22:25:44 -0800

    Added first test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,47 @@ import Display from './components/Display'
 import Dashboard from './components/Dashboard'
 import './App.css';
 
+export const testaddCount = (score, target) =>{
+  let newScore = score;
+  console.log('test here', newScore, target)
+  if (target === 'ball'){
+    console.log ('Adding ball')
+    if (newScore.ball === 3 ){
+      newScore.ball = 0;
+      newScore.strike = 0;
+      newScore.foul = 0;
+    }else {
+      newScore.ball++;
+      console.log('ball is now',newScore.ball)
+    }
+  }
+  if (target === 'strike'){
+    console.log ('Adding strike')
+    if (newScore.strike === 2 ){
+      newScore.ball = 0;
+      newScore.strike = 0;
+      newScore.foul = 0;
+    }else {
+      newScore.strike++;
+    }
+  }
+  if (target === 'foul'){
+    console.log ('Adding foul')
+    if (newScore.strike < 2 ){
+      newScore.strike++
+      newScore.foul++
+    }else {
+      newScore.foul++
+    }
+  }
+  if (target === 'hit'){
+    newScore.ball = 0;
+    newScore.strike = 0;
+    newScore.foul = 0;
+  }
+  return (newScore)
+}
+
 class App extends React.Component{
   constructor(){
     super();
@@ -16,7 +57,7 @@ class App extends React.Component{
     }
   }
 
-    addCount = (target) =>{
+  addCount = (target) =>{
     let newScore = this.state.score
     if (target === 'ball'){
       console.log ('Adding ball')
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from '@testing-library/react';
+import App, {testaddCount} from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+const newScore = {
+  ball: 0,
+  strike: 0,
+  foul: 0
+  }
+
+test('adds ball', () => {
+  expect(testaddCount(newScore, 'ball')).toStrictEqual({
+    ball: 1,
+    strike: 0,
+    foul: 0
+  });
+})
